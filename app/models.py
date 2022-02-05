6300d309508056fb18d865ebc53e9d3fab18348c
Josh Katz <joshpk.105@gmail.com>
2022-02-05 21:16:36 +0000

    Working user_login

--- app/models.py
+++ app/models.py
@@ -1,19 +1,32 @@
+from app import db, login
 from datetime import datetime
-from app import db
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.Unicode(64), index=True, unique=True)
     email = db.Column(db.Unicode(320), index=True, unique=True)
     password_hash = db.Column(db.Unicode(128))
-    books = db.relationship('Book', back_populates='owners', lazy='dynamic')
-
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+    # Shouldn't this be done on the users computer in javascript?
+    def set_password(self, pw):
+        self.password_hash = generate_password_hash(pw)
+    
+    # This function should be taking a hash?
+    def check_password(self, pw):
+        return check_password_hash(self.password_hash, pw)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 class Author(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Unicode(200))
+    #books = db.relationship('Book', backref='author', lazy='dynamic') 
 
     def __repr__(self):
         return '<Author {}>'.format(self.name)
@@ -22,18 +35,17 @@ class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.Unicode(120))
     # When referenceing in ForeignKey must use lower-case?
-    author_id = db.Column(db.Integer, db.ForeignKey('author.id'))
-    purchase_datetime = db.Column(db.DateTime, default=datetime.utcnow)
-    owners = db.relationship('User', back_populates='books', lazy='dynamic')
+    owner = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
         return '<Book {}>'.format(self.title)
 
-class Note(db.Model):
+class Ownership(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    purchase_datetime = db.Column(db.DateTime, default=datetime.utcnow)
+    note = db.Column(db.UnicodeText)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     book_id = db.Column(db.Integer, db.ForeignKey('book.id'))
-    note = db.Column(db.UnicodeText)
 
     def __repr__(self):
-        return '<Note {}>'.format(note[0:10])
+        return '<Ownership {}>'.format(purchase_datetime)
--- assignment.py
+++ assignment.py
@@ -1,1 +1,6 @@
-from app import app
+from app import flapp, db
+from app.models import User, Book, Author, Ownership
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Book': Book, 'Author': Author, 'Ownership': Ownership}
