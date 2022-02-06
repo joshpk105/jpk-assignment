3e7a4be0e0cfeee294ece7143d8c6ddb54b7517d
Josh Katz <joshpk.105@gmail.com>
2022-02-06 00:43:32 +0000

    Authentication blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -4,12 +4,28 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
-flapp = Flask(__name__)
-flapp.config.from_object(Config)
-db = SQLAlchemy(flapp)
-migrate = Migrate(flapp, db)
-login = LoginManager(flapp)
-login.login_view = 'login'
+db = SQLAlchemy()
+migrate = Migrate()
+login = LoginManager()
+login.login_view = "auth.login"
+login.login_message = 'Please log in to access this page.'
+
+def create_app(config_class=Config):
+    flapp = Flask(__name__)
+    flapp.config.from_object(Config)
+
+    # Initilize extensions
+    db.init_app(flapp)
+    migrate.init_app(flapp)
+    login.init_app(flapp)
+
+    # Blueprints
+    from app.auth import bp as auth_bp
+    flapp.register_blueprint(auth_bp, url_prefix="/auth")
+
+    from app.main import bp as main_bp
+    flapp.register_blueprint(main_bp)
+
+    return flapp
 
 from app import models
-from app import routes
